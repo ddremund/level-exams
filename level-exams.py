17670a2578db3facb17692b14205b84edb9c80ae
Derek Remund <ddremund@gmail.com>
2013-05-02 14:20:12 -0500

    debugging test deletion

--- level-exams.py
+++ level-exams.py
@@ -338,16 +338,15 @@ def delete_saved_test(test_id):
     test = saved_tests.get_test(test_id)
     
     if not test['username'] == username and not username == 'ent-leveling':
-        tests = saved_tests.get_all_tests()
-        tests.sort(key = lambda item: item['timestamp'])
-        return bottle.template('saved_tests', dict(username = username, 
-                                tests = tests, errors = "Test created by another user"))
+        errors = "Test created by another user"
     else:
         saved_tests.remove_test(test_id)
-        tests = saved_tests.get_all_tests()
-        tests.sort(key = lambda item: item['timestamp'])
-        return bottle.template('saved_tests', dict(username = username, 
-                                tests = tests, errors = "Test deleted successfully"))
+        errors = "Test deleted successfully"
+        
+    tests = saved_tests.get_all_tests()
+    tests.sort(key = lambda item: item['timestamp'])
+    return bottle.template('saved_tests', dict(username = username, 
+                            tests = tests, errors = errors))
 
 
 @bottle.get('/internal_error')
