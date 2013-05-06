5cbafd00b28321f274cade391e876d7a72d1fcc8
Derek Remund <ddremund@gmail.com>
2013-05-06 13:20:18 -0500

    marshal dup_ids as ObjectIds

--- questionDAO.py
+++ questionDAO.py
@@ -84,6 +84,7 @@ class QuestionDAO:
 
 	def get_questions(self, topic, level, num_questions = 0, dup_ids = []):
 
+		dup_ids = [ObjectId(id) for id in dup_ids]
 		if num_questions == 0:
 			cursor = self.questions.find({"topic": topic, "levels": level})
 		else:
